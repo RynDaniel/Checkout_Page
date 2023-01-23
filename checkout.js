6b7cfafc7554c55780137e7fb2caac1af8a4942c
RynDaniel <ryan@clarusway.com>
2023-01-23 22:03:19 +0300

    second lesson commit

--- checkout.js
+++ checkout.js
@@ -17,17 +17,30 @@ productsDiv.addEventListener("click", (e)=>{
     // alert(e.target.tagName);
     // alert(e.currentTarget.className);
     if(e.target.className === "fa-solid fa-minus"){
-        alert("minus btn clicked");
-        calculateProductPrice(e.target);
+        // alert("minus btn clicked");
+        if(e.target.nextElementSibling.innerText > 1){
+            e.target.nextElementSibling.innerText--;
+            calculateProductPrice(e.target);
+        }
+        else{
+            //innerText vs. textContent(whitespaces)
+            if(confirm(`${e.target.closest(".product-info").querySelector("h2").innerText} will be removed!`)){
+                e.target.closest(".product").remove();
+            }
+        }
         calculateCartPrice();
     }
     else if(e.target.classList.contains("fa-plus")){
-        alert("plus btn clicked");
+        // alert("plus btn clicked");
+        e.target.parentElement.querySelector(".quantity").innerText++;
         calculateProductPrice(e.target);
         calculateCartPrice();
     }
     else if(e.target.getAttribute("class") == "remove-product"){
-        alert("remove btn clicked");
+        // alert("remove btn clicked");
+        if(confirm(`${e.target.closest(".product-info").querySelector("h2").innerText} will be removed!`)){
+            e.target.closest(".product").remove();
+        }
         calculateCartPrice();
     }
     else{
@@ -36,16 +49,22 @@ productsDiv.addEventListener("click", (e)=>{
 
 });
 
-const calculateProductPrice = (target) =>{
+const calculateProductPrice = () =>{
     //each product total calculation
     //productTotalPrice => quantity * price
     const productInfoDiv = target.closest(".product-info");
     console.log(productInfoDiv);
+    //unit price
+    //div.class vs. .class as performance
+    const price = productInfoDiv.querySelector("div.product-price strong").innerText;
+    //quantity
+    const quantity = productInfoDiv.querySelector("p.quantity").innerText;
+    productInfoDiv.querySelector("div.product-line-price").innerText = (price * quantity).toFixed(2);
 }
 
 const calculateCartPrice = () =>{
     //cart total calculation from all products
-
+    
 }
 
 
